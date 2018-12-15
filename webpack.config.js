911c68df2fed74d482179fd93f7e77ee38a31038
Alexandru Cambose <alexcambose@yahoo.com>
2018-12-15 16:33:25 +0200

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'motus.js',
+  },
+  devtool: 'sourcemaps',
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+  },
+};
